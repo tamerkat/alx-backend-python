c876ace1cf97b3b75126d6968266b19b3a9ef531
tamerkat <mahad10taha@gmail.com>
2024-08-06 18:27:34 +0300

    add

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,5 +8,6 @@ from typing import List
 
 
 async def wait_n(n: int = 0, max_delay: int = 10) -> List[float]:
-    tasks = [asyncio.create_task(wait_random(max_delay) for _ in range(n))]
+    """block of code"""
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
     return (await task for task in asyncio.as_completed(tasks))
