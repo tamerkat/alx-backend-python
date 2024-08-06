af37bf7602cc9bfa7b6c4042be95fcdfb60f5e07
tamerkat <mahad10taha@gmail.com>
2024-08-06 16:46:28 +0300

    add

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
+from typing import List
 """model def"""
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     sum: float = 0.0
     for i in input_list:
         sum += i
