fa04cf6394e6050f775dbf31b463f0b1068a6b3e
tamerkat <mahad10taha@gmail.com>
2024-08-06 17:21:46 +0300

    add

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -2,9 +2,9 @@
 """model def"""
 
 
-from typing import Union
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """block of code"""
     return k, v
