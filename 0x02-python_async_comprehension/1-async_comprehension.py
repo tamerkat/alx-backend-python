f4b15f38f3924a1bfa8060359c3619a2e9627526
tamerkat <mahad10taha@gmail.com>
2024-08-07 13:34:38 +0300

    add

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -6,10 +6,11 @@
 
 import random
 import asyncio
+from typing import List
 
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     return [number async for number in async_generator()]
